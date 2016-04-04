6e386039410f5dedcbdbb9d9219a9eb3829c3ffe
Chris Blackburn <krizboy12@gmail.com>
2016-04-03 23:43:42 -0400

    Minor bug fix
    fixed moves not printing

--- src/Game/Move.java
+++ src/Game/Move.java
@@ -19,11 +19,11 @@ public class Move {
 	
 	// simple movement
 	public void executeCurrentMove() {
+		System.out.println(this);
+		
 		board.getTile(destination).setPiece(board.getTile(pos).getPiece());
 		board.getTile(pos).setPiece(null);
 		board.getTile(destination).getPiece().setPos(destination);
-		
-		//System.out.println(this);
 	}
 	
 	// simple implementation of move representation
